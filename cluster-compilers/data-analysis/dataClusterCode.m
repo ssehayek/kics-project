bc15fda3531a82fed274bf2d97f820d3d4085c0d
ssehayek <simon.sehayek@mail.mcgill.ca>
2016-10-07 01:07:15 -0400

    changes to data-analysis codes added time-integration and ics constraints (see changes to simulation cluster codes)

--- cluster-compilers/data-analysis/dataClusterCode.m
+++ cluster-compilers/data-analysis/dataClusterCode.m
@@ -1,7 +1,8 @@
 %% preliminary code
 
-rng shuffle % reseed the random number generator; otherwise same random
-            % numbers are generated each time MATLAB restarts
+% reseed the random number generator; otherwise same random
+% numbers are generated each time MATLAB restarts
+rng shuffle 
 
 run('getAnalysisInput') % gets all parameters from "analysisInput.txt.mv"
 
@@ -54,9 +55,13 @@ load(loadPath)
 analysisDir = [runDir,filesep,'analysis'];
 mkdir(analysisDir)
 
+% for convention
+J = loadedMovie;
+clear loadedMovie
+
 %% fit intensity trace
 
-[p,ci,bleach_fig] = bleachFit(loadedMovie)
+[p,ci,bleach_fig] = bleachFit(J)
 k_p_fit = p(2); % fitted value for bleaching rate
 
 % save intensity trace with bleach profile fit
@@ -69,21 +74,83 @@ saveas(bleach_fig,filename)
 bleach_fit_info.opt_params = p;
 bleach_fit_info.ci = ci;
 
-%% fitting
+%% compute kICS autocorr
 
-kSqMinIndex = find(kSqVector >= kSqMin,1,'first') % lowest index, i, which satisfies kSqVector(i) >= kSqMin
-if strcmpi(kSqMax,'max') % highest index, j, which satisfies kSqVector(j) <= kSqMax
-    kSqMaxIndex = length(kSqVector); % use entire |k|^2 vector
-else
-    kSqMaxIndex = find(kSqVector <= kSqMax,1,'last') 
+if redo_kics
+    kSqVector = getKSqVector(J);
+    [kSqVectorSubset,kSqSubsetInd] = getKSqVector(J,'kSqMin',kSqMin,'kSqMax',kSqMax);
+    
+    r_k_circ_uncut = zeros(length(kSqVector),length(tauVector));
+    r_k_circ = zeros(length(kSqVectorSubset),length(tauVector));
+    
+    %
+    tic
+    
+    r_k_norm = kICS3(J-repmat(mean(J,3),[1,1,size(J,3)]),'normByLag',normByLag); % kICS autocorrelation function (ACF)
+    if do_interp
+        n_theta_arr = n_theta*ones(1,length(kSqVectorSubset));
+        
+        r_k_tau = r_k_norm(:,:,tauVector+1);
+        parfor tau_i = 1:length(tauVector)
+            r_k_circ(:,tau_i) = ellipticInterp(r_k_tau(:,:,tau_i),kSqVectorSubset,n_theta_arr);
+        end
+        delete(gcp)
+    else
+        r_k_circ_uncut = circular(r_k_norm(:,:,tauVector+1));
+        r_k_circ = r_k_circ_uncut(kSqSubsetInd,:);
+    end
+    r_k_abs = abs(r_k_circ); % get rid of complex values
+    
+    toc
+    %
+    
+    % if any(strcmpi(normByLag,{'none','noNorm',''})) % some normalization for when the kICS AC is not normalized, otherwise the fit is unreasonable
+    %     max_value = max(max(r_k_abs));
+    %     r_k_abs = r_k_abs/max_value;
+    % end
+    
+    cd(runDir)
+    filename = [runDir filesep 'analysis' filesep 'kICS_Data.mat'];
+    save(filename,'kSqVector','r_k_abs','r_k_norm'); % save computed kICS ACF
 end
-kSqVectorSubset = kSqVector(kSqMinIndex:kSqMaxIndex); % all values satisfying kSqMin <= kSqVector <= kSqMax
-kSqSubsetInd = kSqMinIndex:kSqMaxIndex; % all indices satisfying kSqMin <= kSqVector(i) <= kSqMax
 
-kICSCorrSubset = r_k_abs(kSqSubsetInd,:); % corresponding subset of kICS ACF
+%% ICS for guesses
+
+if use_ics_guess
+    ics_figpath = [runDir filesep 'analysis' filesep 'ics_figs'];
+    ics_run = ICSCompiler(J,xi_lags,eta_lags,'subsets',n_ics_subs,...
+        'saveFigs',ics_figpath);
+    
+    % save ICS info
+    filename = [runDir filesep 'analysis' filesep 'ics_info.mat'];
+    save(filename,'ics_run')
+    
+    % compute tau = 0 lag in kICS
+    n_theta_arr = n_theta*ones(1,length(kSqVector));
+    
+    r_k_tau_0 = kICS3(J-repmat(mean(J,3),[1,1,size(J,3)]),'normByLag','none'); % kICS autocorrelation unnormalized
+    r_k_circ_0 = ellipticInterp(r_k_tau_0(:,:,1),kSqVector,n_theta_arr);
+    %
+    
+    % get param guesses for kICS fitting
+    kics_figpath = [runDir filesep 'analysis'];
+    [params_guess,lb,ub] = getkICSGuess(J,ics_run,p,kSqVector,r_k_circ_0,...
+        ksq_noise_lb,'saveFigs',kics_figpath);
+    %
+    
+    % save kICS guesses
+    filename = [runDir filesep 'analysis' filesep 'kics_guesses.mat'];
+    save(filename,'params_guess','lb','ub')
+    %
+end
+
+%% kICS fitting
+
+% for convention
+kICSCorrSubset = r_k_abs; % corresponding subset of kICS ACF
 
 T = length(session_info.tRange);
-err = @(params) kICSNormTauFitFluctNoiseBleach(params,kSqVectorSubset,tauVector,...
+err = @(params) timeIntkICSFit(params,kSqVectorSubset,tauVector,...
     k_p_fit,T,'normByLag',normByLag,'err',kICSCorrSubset); % function handle of LSF error
 
 tic
@@ -121,8 +188,10 @@ disp(['minimum objective function: ',num2str(err_min)])
 %     disp(['true parameters: ',num2str(sim_info.true_params)])
 
 % get confidence intervals
-x0 = opt_params; % set initial guess to best global fit to get immediate
-                 % convergence and correct Hessian from "fminunc.m"
+%
+% set initial guess to best global fit to get immediate
+% convergence and correct Hessian from "fminunc.m"
+x0 = opt_params; 
 
 [x,~,~,~,~,hessian] = fminunc(err,x0);
 disp(['params from "fminunc.m":',num2str(x)])
@@ -147,7 +216,7 @@ color = lines(length(plotTauLags));
 plotLegend = cell(1,length(plotTauLags));
 h_sim_data = zeros(1,length(plotTauLags));
 for tauInd = 1:length(plotTauLags) % loop and plot over fixed time lag
-    h_sim_data(tauInd) = plot(kSqVectorSubset,kICSCorrSubset(:,plotTauLags(tauInd)+1),'.','markersize',16,'Color',color(tauInd,:)); % plot heuristic kICS ACF
+    h_sim_data(tauInd) = plot(kSqVectorSubset,kICSCorrSubset(:,tauInd),'.','markersize',16,'Color',color(tauInd,:)); % plot heuristic kICS ACF
     plotLegend{tauInd} = ['$\tau = ' num2str(plotTauLags(tauInd)) '$'];
 end
 % labeling
@@ -168,7 +237,7 @@ saveas(gcf,filename)
 %% plot best fit curves
 
 for tauInd = 1:length(plotTauLags) % loop and plot over fixed time lag
-    plot(ksq2plot,kICSNormTauFitFluctNoiseBleach(opt_params,ksq2plot,plotTauLags(tauInd),...
+    plot(ksq2plot,timeIntkICSFit(opt_params,ksq2plot,plotTauLags(tauInd),...
         k_p_fit,T,'normByLag',normByLag),'Color',color(tauInd,:)) % plot best fit kICS ACF
 end
 tightfig(gcf)
